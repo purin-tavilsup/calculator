d7ec832a4d583a2f6c037c172845c7fe52004e7c
KorntanaJ <korntana.j@gmail.com>
2023-12-29 12:19:35 -0600

    adjust logic by comment

--- Calculator/Calculator/src/Calculator.cs
+++ Calculator/Calculator/src/Calculator.cs
@@ -14,7 +14,7 @@
 
 		public double Multiply(double value1, double value2)
 		{
-			return (value1 * value2);
+			return value1 * value2;
 		}
 
 		public double Divide(double value1, double value2)
